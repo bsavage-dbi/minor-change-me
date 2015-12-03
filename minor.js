c4577cdb3770ccb6849ac4b64ff536a0c4cff5c1
Thiago Porciúncula <thiago.porciuncula@softplan.com.br>
2015-12-03 08:13:46 -0200

    Add support for multiple versions of Confluence

--- minor.js
+++ minor.js
@@ -2,3 +2,8 @@ minorEdit = document.getElementById("minorEdit");
 if (minorEdit) {
   minorEdit.checked = true;
 }
+
+notifyWatchers = document.getElementById("notifyWatchers");
+if (notifyWatchers) {
+  notifyWatchers.checked = false;
+}
